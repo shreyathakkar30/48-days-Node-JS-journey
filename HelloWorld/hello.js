66b683c99578f545c8ecd109284fd2956dd5320d
Shreya <shreyathakkar2004@gmail.com>
2026-06-04 12:10:09 +0530

    HelloWorld from the NodeJS

---
+++ HelloWorld/hello.js
@@ -0,0 +1,16 @@
+console.log("Hey There I am JS");
+
+//following will Throw an error when you run those in terminal whereas successfully executed when executed in Browser
+//Jitni bhi DOM and Window k related chije hai use nikal diya gaya NodeJs me
+//And boht sare features ko add bhi kiya gaya For Example, 
+//Jaise ki kisi chij ko Encrypt krna hai, Decrypt krna hai, Hash krna hai to ya fir file Handling hai vo sab chijo ko isme add ki gai hai
+//And jitna bhi DOM k related Work hai jo ki hume Server Side nahi chahiye usko remove kr diya gaya hai
+//console.log(window);
+//console.log(alert('Hey'));
+// to run this or to show the o/p run the command in the Terminal:
+// node hello or node hello.js
+// Here .js is optional because node always executes JS files itself
+// NPM stands for Node Package Manager
+// use command npm init 
+// to ye krta kya hai => humare liye 1 file/template banata hai jo humare project ko run krne me kaam aati hai
+//It will create Packet.json file
