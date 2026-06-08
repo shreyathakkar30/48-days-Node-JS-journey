9b6a01acbfd7f8a93744ad6775b0410cafb4f904
Shreya <shreyathakkar2004@gmail.com>
2026-06-08 22:35:30 +0530

    Status Codes in HTTP

--- Project-01/index.js
+++ Project-01/index.js
@@ -45,15 +45,19 @@ app.get('/api/users', (req, res) => {
 app.get('/api/users/:id', (req,res) => {
     const id = Number(req.params.id);
     const user = users.find(users => users.id === id);
+    if(!user) return res.status(404).json({ error : "user not found"});
     return res.json(user);
 });
 
 app.post('/api/users', (req, res) => {
     //TO DO Create New User
     const body = req.body;
+    if(!body || !body.first_name || !body.last_name || !body.email || !body.gender || !body.job_title){
+        return res.status(400).json({msg: "All fields are mandatory"});
+    }
     users.push({...body, id: users.length + 1});
     fs.writeFile('./MOCK_DATA.json', JSON.stringify(users), (err, data) =>{
-        return res.json({status : "success", id: users.length});
+        return res.status(201).json({status : "success", id: users.length});
     })
     
 })
